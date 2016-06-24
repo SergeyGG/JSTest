6023261fe48ca4525fa221f3c5168a954320c41b
SergeyGG <1letterssk@gmail.com>
2016-06-24 09:27:38 +0300

    edit

--- src/js2.js
+++ src/js2.js
@@ -3,3 +3,13 @@ function take(gen, x) {
 	alert( gen() );
 	}
 }
+
+function sequence(start, step) {
+	start = start || 0;
+	step = step || 1;
+	start -= step; 
+	
+	return function() {
+		return start += step;
+	}
+}
