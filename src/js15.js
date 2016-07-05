6c0006f7504ddabbd948d1c8e528829e9c22e5f8
SergeyGG <1letterssk@gmail.com>
2016-07-05 11:22:30 +0300

    edit

--- src/js15.js
+++ src/js15.js
@@ -1,5 +1,10 @@
 function shallowCopy(obj){
   var ObjectCopy = [];
+  
+  if(obj instanceof Date){
+    return new Date(obj);
+  }
+  
   for(var key in obj) {
     ObjectCopy[key] = obj[key];
   }
@@ -15,3 +20,8 @@ console.log(a.x); // 1
 // Но a.z и b.z указывают на один и тот же массив: 
 b.z.push(4);
 console.log(a.z); // [1, 2, 3, 4]
+
+var с = new Date(2014, 1, 1);
+var d = shallowCopy(c);
+d.setFullYear(2015);
+console.log(c.getFullYear()); // 2014
